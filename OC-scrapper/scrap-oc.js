6d95d0178f6e112402e9cb4b8c82cd66d2baf7f5
nicolaspatschkowski <nicolas.patschkowski@openclassrooms.com>
2022-09-06 13:16:25 +0200

    Reinit DOM when re-uploading

--- OC-scrapper/scrap-oc.js
+++ OC-scrapper/scrap-oc.js
@@ -6,29 +6,31 @@ function readSingleFile(e) {
   var reader = new FileReader();
   reader.onload = function(e) {
     var contents = e.target.result;
+    //Add content of the fil to DOM
+    let temp = document.createElement('fileContent');
+    temp.innerHTML = contents;
+    jQuery('#fileContent_container').empty().html(temp);
     displayProjects(contents);
   };
   reader.readAsText(file);
 }
 
-function displayContents(contents) {
-  var element = document.getElementById('file-content');
-  element.textContent = contents;
+function createPathTitleElement(pathTitle, pathID) {
+  //building the proper URLs :
+  var pathURL = `https://openclassrooms.com/fr/paths/${pathID}`;
+  var pathEditURL = `https://openclassrooms.com/fr/admin/paths/${pathID}/edit`;
+
+  //Creating element for the path and add to DOM
+  var pathElemString = `<a href="${pathURL}">${pathTitle}</a> - <a href="${pathEditURL}">edit link</a>`;
+  var pathElem = jQuery(pathElemString);
+
+  jQuery('#path_container').append(pathElem);
 }
 
-
-function displayProjects(contents) {
-
-  let temp = document.createElement('fileContent');
-  temp.innerHTML = contents;
-  jQuery('#fileContent-container').append(temp);
-
-  var projectData = jQuery('[data-react="AutocompleteMultiple"').attr('data-props');
-  var projectDataObject = JSON.parse(projectData).defaultValues;
+function createProjectList(projectDataObject) {
   
-  var projectListElem = jQuery('#file-projects');
+  var projectListElem = jQuery('#file_projects');
   for (var i = 0 ; i < projectDataObject.length; i++) {
-    console.log(i);
     //building the proper URLs and title :
     var projectID = projectDataObject[i].value;
     var projectURL = "https://openclassrooms.com/fr/projects/" + projectID + "/assignment";
@@ -36,13 +38,32 @@ function displayProjects(contents) {
     var replaceString = ` (${projectID})`;
     var projectTitle = projectDataObject[i].label.replace(replaceString, "");
     
-    //Creating element for the project to add to the list
+    //Creating element for the project to add to the list in DOM
     var projectElemString = `<li>P${i+1} (${projectID}) : <a href="${projectURL}">${projectTitle}</a> - <a href="${editURL}">edit link</a></li>`;
     var projectElem = jQuery(projectElemString);
+
     projectListElem.append(projectElem);
   }
+}
 
+function displayProjects(contents) {
+
+  //Reset DOM
+  jQuery('#path_container').empty();
+  jQuery('#file_projects').empty();
+
+  //Create path title
+  var pathTitle = jQuery('#edit_learning_path_title').attr('value');
+  var pathID = jQuery('#edit_learning_form').attr('action').match(/[0-9]+/)[0];
+  createPathTitleElement(pathTitle, pathID);
+
+
+  //Create project list
+  var projectData = jQuery('[data-react="AutocompleteMultiple"').attr('data-props');
+  var projectDataObject = JSON.parse(projectData).defaultValues;
+
+  createProjectList(projectDataObject);
 }
 
 
-document.getElementById('file-input').addEventListener('change', readSingleFile, false);
+document.getElementById('file_input').addEventListener('change', readSingleFile, false);
